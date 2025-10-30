0cd0dfa8084acaf6d7e8a08c8db3db297d5e9cfd
amarjeet-choudhary666 <amarjeetchoudhary747@gmail.com>
2025-10-30 19:35:25 +0530

    first commit

--- dist/routes/experienceRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const experienceController_1 = require("../controllers/experienceController");
-const router = (0, express_1.Router)();
-router.get("/", experienceController_1.getExperiences);
-router.get("/:id", experienceController_1.getExperienceById);
-exports.default = router;
-//# sourceMappingURL=experienceRoutes.js.map
--- dist/routes/promoRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const promoController_1 = require("../controllers/promoController");
-const router = (0, express_1.Router)();
-router.post("/validate", promoController_1.validatePromo);
-exports.default = router;
-//# sourceMappingURL=promoRoutes.js.map
